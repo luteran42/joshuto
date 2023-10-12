dc0b1d19eac8d79d3ccf9df3caa1b3dd1f2934d0
luteran42 <luteranlajos@protonmail.ch>
2023-10-12 21:59:58 +0200

    use english

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 cargo zigbuild --target x86_64-unknown-linux-musl --release --locked
 cp -f ./target/x86_64-unknown-linux-musl/release/joshuto ~/.local/bin/cargo/bin/
-echo "joshuto telepítve: ~/.local/bin/cargo/bin/joshuto"
+echo "Installed joshuto: ~/.local/bin/cargo/bin/joshuto"
