35cc5cee17e0ce0f9993b198676376fadda9e918
Luterán Lajos <luteranlajos@protonmail.ch>
2024-07-06 20:20:11 +0200

    reverted 85ba4a1af5181494b066f61ca2efdcb32465db76 so musl build works again??
    Signed-off-by: Luterán Lajos <luteranlajos@protonmail.ch>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-# cargo zigbuild --target x86_64-unknown-linux-musl --release --locked
-cargo build --release --locked
-# cp -f ./target/x86_64-unknown-linux-musl/release/joshuto ~/.local/bin/cargo/bin/
-cp -f ./target/release/joshuto ~/.local/bin/cargo/bin/
+cargo zigbuild --target x86_64-unknown-linux-musl --release --locked
+# cargo build --release --locked
+cp -f ./target/x86_64-unknown-linux-musl/release/joshuto ~/.local/bin/cargo/bin/
+# cp -f ./target/release/joshuto ~/.local/bin/cargo/bin/
 echo "Installed joshuto: ~/.local/bin/cargo/bin/joshuto"
