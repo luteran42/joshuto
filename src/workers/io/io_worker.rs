a661443d8997f1fa2c86c1d28ac78b5f49a75c00
Luterán Lajos <luteranlajos@protonmail.ch>
2024-07-20 17:26:33 +0200

    lali-joshuto: i guess i dont need cancel error msg in io_worker.rs
    Signed-off-by: Luterán Lajos <luteranlajos@protonmail.ch>

--- src/workers/io/io_worker.rs
+++ src/workers/io/io_worker.rs
@@ -43,46 +43,10 @@ impl IoWorkerThread {
 
     pub fn start(&self, tx: mpsc::Sender<IoWorkerProgressMessage>) -> AppResult<()> {
         match self.get_operation_type() {
-            FileOperation::Cut => {
-                if self.options.cancel {
-                    Err(AppError::new(
-                        AppErrorKind::UnknownError,
-                        "Cut: operation cancelled!".to_string(),
-                    ))
-                } else {
-                    self.paste_cut(tx)
-                }
-            }
-            FileOperation::Copy => {
-                if self.options.cancel {
-                    Err(AppError::new(
-                        AppErrorKind::UnknownError,
-                        "Copy: operation cancelled!".to_string(),
-                    ))
-                } else {
-                    self.paste_copy(tx)
-                }
-            }
-            FileOperation::Symlink { relative: false } => {
-                if self.options.cancel {
-                    Err(AppError::new(
-                        AppErrorKind::UnknownError,
-                        "Link(absolute): operation cancelled!".to_string(),
-                    ))
-                } else {
-                    self.paste_link_absolute(tx)
-                }
-            }
-            FileOperation::Symlink { relative: true } => {
-                if self.options.cancel {
-                    Err(AppError::new(
-                        AppErrorKind::UnknownError,
-                        "Link(relative): operation cancelled!".to_string(),
-                    ))
-                } else {
-                    self.paste_link_relative(tx)
-                }
-            }
+            FileOperation::Cut => self.paste_cut(tx),
+            FileOperation::Copy => self.paste_copy(tx),
+            FileOperation::Symlink { relative: false } => self.paste_link_absolute(tx),
+            FileOperation::Symlink { relative: true } => self.paste_link_relative(tx),
             FileOperation::Delete => self.delete(tx),
         }
     }
