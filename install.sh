fb56b3d607b19d297657b5139871c76fe303c9b0
Luterán Lajos <luteranlajos@protonmail.ch>
2024-08-13 16:57:35 +0200

    lali: update install script
    Signed-off-by: Luterán Lajos <luteranlajos@protonmail.ch>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 rustup default nightly
-cargo zigbuild --target x86_64-unknown-linux-musl -Z build-std=std,panic_abort -Z build-std-features= --release --locked
+cargo zigbuild --target x86_64-unknown-linux-musl --release --locked
 cp -f ./target/x86_64-unknown-linux-musl/release/joshuto ~/.local/bin/cargo/bin/
 echo "Installed joshuto: ~/.local/bin/cargo/bin/joshuto"
 rustup default stable
