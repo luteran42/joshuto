155753404ab00bb64219ac4071860d8d94bf5ee2
Luterán Lajos <luteranlajos@protonmail.ch>
2024-09-28 12:10:22 +0200

    lali: add debug to install.sh
    Signed-off-by: Luterán Lajos <luteranlajos@protonmail.ch>

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+
+set -xe
+
 rustup default nightly
 cargo zigbuild --target x86_64-unknown-linux-musl --release --locked
 cp -f ./target/x86_64-unknown-linux-musl/release/joshuto ~/.local/bin/cargo/bin/
